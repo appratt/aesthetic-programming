8bfd1ae29690ae732cfaed763ae8142a3543f98c
Andrew Pratt <appratt@gmail.com>
2022-04-24 20:52:06 -0400

    setup repository to allow work on multiple laptops

---
+++ README.md
@@ -0,0 +1,3 @@
+# READ ME
+
+This repository is for working through the examples in the Aesthetic Programming book.
---
+++ p5/myFirstSketch/sketch02.js
@@ -0,0 +1,20 @@
+function setup() {
+  // put setup code here
+  createCanvas(640, 480);
+}
+
+function draw() {
+  // put drawing code here
+  // R, G & B integer values
+  stroke(0, 153, 51);
+  strokeWeight(4);
+  fill(102, 153, 255);
+  rect(20, 20, 60, 60);
+  translate(30, 20);
+  rect(20, 20, 60, 60);
+  translate(60, 40);
+  rect(20, 20, 60, 60);
+  describe('White rect at center with yellow outline.');
+}
+
+// w3schools color picker: https://www.w3schools.com/colors/colors_picker.asp
